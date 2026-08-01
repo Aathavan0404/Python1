a68493dc5c01c7c9d504d57677a88a50d6c27399
Aathavan0404 <easwaranaathavan@gmail.com>
2026-08-01 10:36:54 +0530

    Refactor Marks validation logic in if else.py for clarity and correctness

--- if else.py
+++ if else.py
@@ -1,13 +1,16 @@
 print("Welcome to the Marks Calculator")
 Marks = float(input("Enter your marks: "))
-if Marks >= 75:
+if 0<= Marks <= 100:
+  if Marks >= 75:
     print("Result = A")
-elif Marks >= 65:
+  elif Marks >= 65:
     print("Result = B")
-elif Marks >+ 50:
+  elif Marks >+ 50:
     print("Result = C")
-elif Marks >= 35:
+  elif Marks >= 35:
     print("Result = S")
-else:
+  else:
     print("Result = W")
+else:
+    print("Invalid Marks")
     
