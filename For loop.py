ad9035cf45bf25d69f69ddd4cf85657e635b27c3
Aathavan0404 <easwaranaathavan@gmail.com>
2026-08-02 18:04:48 +0530

    Add missing for loop example to demonstrate range usage

--- For loop.py
+++ For loop.py
@@ -7,6 +7,9 @@ for i in range (1,10,2):  #1 to 9 with step 2 (Start,Stop.Step) Numbers count = 
 for j in range (1000,0,-10): #Step=-10
     print(j)
 
+for a in range(10):
+    print(a)
+
 Materials = ["Phone","Laptop","Tablet"]   #List
 for M in Materials:
     print(M)
