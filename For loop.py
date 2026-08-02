5c3f977e5e6879d86e965a0916891c1170705e40
Aathavan0404 <easwaranaathavan@gmail.com>
2026-08-02 16:46:57 +0530

    Add additional examples of for loops with lists for demonstration

--- For loop.py
+++ For loop.py
@@ -7,3 +7,10 @@ for i in range (1,10,2):  #1 to 9 with step 2 (Start,Stop.Step) Numbers count = 
 for j in range (1000,0,-10): #Step=-10
     print(j)
 
+Materials = ["Phone","Laptop","Tablet"]   #List
+for M in Materials:
+    print(M)
+
+Marks = [56,98,76,34]
+for A in Marks:
+    print(A) 
