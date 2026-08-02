b8911bf37e515b59d6b7d50a63cde56b112b8d02
Aathavan0404 <easwaranaathavan@gmail.com>
2026-08-02 18:42:48 +0530

    Add initial implementation of type demonstration in types.py

---
+++ types.py
@@ -0,0 +1,34 @@
+a = 2
+b = 2.34
+c = "Aathi"
+d = True
+e = None
+f = [1,2,3]
+g = {1,2,3}
+h = (1,2,3)
+i = {"Name":"Aathi","Age":20}
+j = range(10)
+k = bytes(5)
+l = bytearray(5)
+m = memoryview(bytes(5))
+n = 4-3j
+o = frozenset([1,2,3])
+
+print(type(a)) 
+print(type(b))
+print(type(c))  
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))  
+print(type(l))
+print(type(m))      
+print(type(n))
+print(type(o))  
+
+p = complex(4,9)
+print(p)
