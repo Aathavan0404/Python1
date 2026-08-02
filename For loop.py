d2a0b94b737d4f0577455d2d608c7dcc61ad918b
Aathavan0404 <easwaranaathavan@gmail.com>
2026-08-02 16:05:02 +0530

    Add multi-line comment clarification and new For loop example

---
+++ For loop.py
@@ -0,0 +1,9 @@
+for i in range (1,10):  #1 to 9
+    print(i)
+
+for i in range (1,10,2):  #1 to 9 with step 2 (Start,Stop.Step) Numbers count = (Stop - Start)/Step
+    print(i)
+
+for j in range (1000,0,-10): #Step=-10
+    print(j)
+
