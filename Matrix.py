cdd9bbebb9c7d047a5474d3172fc8da3d9913db1
Aathavan0404 <easwaranaathavan@gmail.com>
2026-08-05 20:34:59 +0530

    Add multiplication table implementation in Matrix.py

---
+++ Matrix.py
@@ -0,0 +1,6 @@
+for row in range (1,4):
+    for col in range (1,4):
+        print(row*col, end = " ") #end means that the next print statement will be printed on the same line, separated by a space
+    print()  # This will print a new line after each row is printed
+
+    
