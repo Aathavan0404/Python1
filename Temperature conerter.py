e392e3da18bf83fc615628f4f5792f7602c35be9
Aathavan0404 <easwaranaathavan@gmail.com>
2026-08-14 13:29:42 +0530

    Add temperature converter implementation in Temperature converter.py

---
+++ Temperature conerter.py
@@ -0,0 +1,10 @@
+Temp = []
+while True:
+    T = float(input("Enter temperature in Celsius: "))
+    F = (T * 9/5) + 32
+    print(f"Temperature in Fahrenheit: {F}")
+    Temp.append(F)
+    print(Temp)
+
+    
+    
