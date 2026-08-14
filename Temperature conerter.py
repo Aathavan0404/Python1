8b1d06c0febc5b5225d0c3b6a90c86227ccd179a
Aathavan0404 <easwaranaathavan@gmail.com>
2026-08-14 18:02:17 +0530

    Refactor temperature converter to handle both Celsius and Fahrenheit inputs with improved user prompts

--- Temperature conerter.py
+++ Temperature conerter.py
@@ -1,10 +1,24 @@
 Temp = []
-while True:
-    T = float(input("Enter temperature in Celsius: "))
-    F = (T * 9/5) + 32
-    print(f"Temperature in Fahrenheit: {F}")
-    Temp.append(F)
-    print(Temp)
+Input = ""
+while Input != "exit": #!means that the loop will continue until the user types "exit"
+    T = float(input("Enter temperature value in Celsius or Fahrenheit: "))
+    Unit = input("Enter the Unit (Celsius or Fahrenheit): ")
+    if Unit == "C" or Unit == "c":
+        F = (T * 9/5) + 32
+        print(f"Temperature in Fahrenheit: {F}")
+        Temp.append((F, Unit))
+    elif Unit == "F" or Unit == "f":
+        C = (T - 32) * 5/9
+        print(f"Temperature in Celsius: {C}")
+        Temp.append((C, Unit))
+    else:
+        print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit ")
+    Input = input("Type 'exit' to quit or press Enter to continue: ")         
+    if Input == "exit":
+         break
+print(Temp)
+
+
 
     
     
